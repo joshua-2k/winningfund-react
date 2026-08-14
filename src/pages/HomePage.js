c80d68ccf96425c02816ab0f3f91e6c36a41129c
joshua-2k <310500913+joshua-2k@users.noreply.github.com>
2026-08-14 14:24:47 +0900

    Apply consolidated Home copy updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -399,6 +399,8 @@ function ShortIntroduction() {
           '위닝펀드는 시장을 함께 공부하고,각자의 관점을 검증하며',
           createElement('br'),
           '근거 있는 투자 판단을 만들어가는 대학 연합투자경제동아리입니다.',
+          createElement('br'),
+          '근거 있는 투자 판단을 만들어가는 대학 연합투자경제동아리입니다.',
         ),
         createElement(
           'div',
